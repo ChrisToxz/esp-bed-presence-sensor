d0b172156b613b34573045c6e1b0d936d78ba5f4
ChrisToxz <chris@toxz.nl>
2024-06-23 10:23:02 +0200

    wip

--- src/helpers.h
+++ src/helpers.h
@@ -1,6 +1,9 @@
 bool ledState = false;
 
 
+
+
+
 void initWiFi(){
   Serial.print("Connecting to WiFi");
   WiFi.mode(WIFI_STA);
@@ -22,8 +25,8 @@ void initWiFi(){
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected! IP:");
-  Serial.print(WiFi.localIP());
+  Serial.print("Connected! IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void initOTA(){
@@ -48,6 +51,68 @@ void initOTA(){
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
+}
+
+
+void setCalibration(float value) {
+    EEPROM.put(0, value);
+    EEPROM.commit(); // Ensure the data is written to EEPROM
+
+    LoadCell.setCalFactor(value);
+}
+
+float getCalibration() {
+    float value;
+    EEPROM.get(0, value);
+    return value;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  Serial.println(topic);
+
+  String topicStr = String(topic);
+  if (topicStr == SET_CALIBRATION_TOPIC) {
+    Serial.println("Set calibration");
+    setCalibration(message.toFloat());
+  } else if (topicStr == "home/bed/sensor/set/offset") {
+    // Handle offset action
+  } else if (topicStr == SET_TARE_TOPIC){
+    Serial.println("Taring..");
+    LoadCell.tareNoDelay();
+    if (LoadCell.getTareStatus() == true) {
+      Serial.println("Tare complete");
+    }
+  }
+}
+
+void initMQTT(){
+  mqtt.setServer(MQTT_SERVER, 1883);
+  mqtt.setCallback(callback);
+}
+
+void connectMQTT() {
+  // Loop until we're reconnected
+  while (!mqtt.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqtt.connect(MQTT_HOSTNAME, MQTT_USER, MQTT_PASS, AVAILABILITY_TOPIC, 2, true, "offline")) {
+      Serial.println("connected");
+      mqtt.publish(AVAILABILITY_TOPIC, "online", true);
+      mqtt.subscribe("home/bedsensor/set/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void resetLED(){
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,65 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
-
+#include <PubSubClient.h>
+#include <EEPROM.h>
+#include <HX711_ADC.h>
 #include "config.h"
-#include "helpers.h"
 
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+#include "helpers.h"
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting up");
 
+  EEPROM.begin(4);
   resetLED();
+
   initWiFi();
   initOTA();
+  initMQTT();
 
+  LoadCell.begin();
+  LoadCell.start(2000, true);
+
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, HX711 not found");
+    ESP.restart();
+  }else{
+    Serial.println(LoadCell.getSamplesInUse());
+    Serial.print("Setting Calibration Factor: ");
+    Serial.println(getCalibration());
+    LoadCell.setCalFactor(getCalibration()); // set calibration value (float)
+    LoadCell.setSamplesInUse(samples);
+    Serial.println("Startup is complete");
+  }
 }
 
 void loop() {
+  static boolean newDataReady = 0;
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+ if (newDataReady) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      newDataReady = 0;
+      mqtt.publish(RAW_TOPIC, (char *)String(i).c_str()); // Publish calibration value
+    }
+
   ArduinoOTA.handle();
+  
+  if (!mqtt.connected()) {
+    connectMQTT();
+  }
+  mqtt.loop();
+
+  mqtt.publish(CALIBRATION_TOPIC, (char *)String(getCalibration()).c_str()); // Publish calibration value
+
+
+  delay(100);
 }
